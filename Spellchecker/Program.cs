c0aa670fcc7e348c32f2ee813ba914d04ccc8e81
Simon Schachinger <s.schachinger@gmail.com>
2022-10-18 18:09:40 +0200

    Projektdateien hinzufügen.

---
+++ Spellchecker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Spellchecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            libLinkedList.ILinkedList<string> linkedList = new libLinkedList.LinkedList<string>();
+        }
+    }
+}
